9c7d458a77ec9fae52a10344735f3e3be6d21370
BDunnCode <135765583+BDunnCode@users.noreply.github.com>
2024-04-26 00:17:41 -0500

    add data fetching, models, db connection method

---
+++ app/lib/utils.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectToDB = async () => {
+
+  connection = {}
+
+  try {
+    if (connection.isConnected) return;
+    const db = await mongoose.connect(process.env.MONGODB);
+    connection.isConnected = db.connection[0].readyState;
+  } catch(error) {
+
+  }
+}
