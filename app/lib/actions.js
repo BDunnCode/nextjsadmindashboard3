d705ec9fc7ddce4e097056f2f86bc835fd7c28b3
BDunnCode <135765583+BDunnCode@users.noreply.github.com>
2024-04-28 15:41:11 -0500

    add addUser server action, fix type on password input

---
+++ app/lib/actions.js
@@ -0,0 +1,24 @@
+import { redirect } from "next/navigation";
+import { User } from "./models";
+import { connectToDB } from "./utils";
+import { revalidatePath } from "next/cache";
+
+export const addUser = async (formData) => { 
+  "use server";
+  const { username, email, password, phone, address, isAdmin, isActive } = Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+    const newUser = new User({
+      username, email, password, phone, address, isAdmin, isActive
+    });
+
+    await newUser.save();
+  } catch(error) {
+    console.log(error)
+    throw new Error("Failed to create user!")
+  }
+
+  revalidatePath("/dashboard/users");
+  redirect("/dashboard/users");
+};
